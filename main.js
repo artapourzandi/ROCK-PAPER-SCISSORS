953d22c31958662c5383f464dd311299a316e460
Arta <artapourzandi@gmail.com>
2021-03-31 17:17:07 +0200

    main.js

--- main.js
+++ main.js
@@ -1,15 +1,59 @@
 // Create an array to randoly choose from
 
-const arr = ["Rock", "Paper", "Scissor"];
+const arr = ["rock", "paper", "scissors"];
 
 // creat a function randomly choose from 'Rock', 'Paper' or 'sciasors'.
 
 function computerPlay() {
-    // let random_pick = Math.floor(Math.random() * arr.length);
-    // console.log(random_pick, arr[random_pick]);
 
     let random_pick = Math.floor(Math.random() * arr.length);
-    console.log(arr[random_pick]);
+    // console.log(arr[random_pick]);
+    return arr[random_pick];
 }
 
-computerPlay()
+let playerSelection = 'Rock';
+let machine = computerPlay();
+console.log(machine);
+
+function playRound(playerSelection, computerSelection) {
+    // const expr = '';
+    switch (computerSelection) {
+      case 'rock':
+        // console.log('rock - rock! Equal');
+        return "rock - rock! Equal";
+        break;
+      case 'paper':
+        // console.log('You Lose! Paper beats Rock');
+        return "You Lose! Paper beats Rock";
+        break;
+      case 'scissors':
+        // console.log('I Lose! rock beats scissors');
+        return "I Lose! rock beats scissors";
+        break;  
+    }
+}
+
+let one_round = playRound(playerSelection, machine);
+console.log(one_round);
+
+const game = () => {
+  player_score = 0; 
+  machine_score = 0;
+  for (let i = 1; i < 6; i++) {
+    let result = playRound(playerSelection, machine);
+      if (result === 'You Lose! Paper beats Rock') {
+        player_score ++;
+      } else if (result === 'I Lose! rock beats scissors') {
+        machine_score ++;
+      }
+  }
+  return machine_score;
+  // if (player_score > machine_score) {
+  //   return (player_score);
+  // } else {
+  //   return machine_score;
+  // }
+};
+
+let final_score = game();
+console.log(final_score);
